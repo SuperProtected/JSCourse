9408a585ae32ef58fdc0900badc759cdd8c18568
Serhii <1077stargonkiff7@gmail.com>
2024-01-25 19:38:22 +0200

    homework 1.2

---
+++ homework1.2.js
@@ -0,0 +1,34 @@
+function CanSmoke(age) {
+
+    if (age >= 18) {
+        console.log("you can smoke")
+    }
+
+    if (age < 18) {
+        console.log("you cant smoke")
+    }
+}
+
+CanSmoke(18);
+
+function myFunction() {};
+
+let a = function () {};
+
+let arrowFunct = () => {};
+
+function allowedTOIn(name, age, department) {
+
+    if (name === "Serhii" || name === "Raman") {
+        console.log("name existing");
+    }
+
+    if (18 <= age) {
+        console.log("age enough");
+    }
+
+    if (department === "Qa" || department === "QA" || department === "qa") {
+        console.log("You Qa Engineer");
+    }
+
+}
