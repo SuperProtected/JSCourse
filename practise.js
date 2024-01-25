715f18dcf8b1ed4f27a23a40291d58583ef4ae9d
Serhii <1077stargonkiff7@gmail.com>
2024-01-25 14:23:11 +0200

    homework 1

---
+++ practise.js
@@ -0,0 +1,13 @@
+console.log(typeof 123);
+console.log(typeof 123n);
+console.log(typeof "Hello");
+console.log(typeof someVarible);
+console.log(typeof null);
+console.log(typeof false);
+console.log(typeof Symbol("s"))
+
+console.log(123 === 123);
+console.log("hello" === 123);
+
+console.log(123 == 123);
+console.log("hello" == 123);
